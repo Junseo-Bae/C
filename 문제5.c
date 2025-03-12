794cd7b1052663434894e2177a4a07c8af5c3779
Junseo-Bae <74636948+Junseo-Bae@users.noreply.github.com>
2025-03-12 18:31:10 +0900

    Create 문제5.c

---
+++ 문제5.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main()
+{
+    int i, j, k;
+
+    scanf("%d", &k);
+
+    for(i = 0; i >= k; i++)
+    {
+        i = i + 1;
+    }
+    printf("%d", i);
+
+    return 0;
+}
+
