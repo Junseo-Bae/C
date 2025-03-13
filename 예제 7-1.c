bf942a8f0e3bd5184c51b81040ca6c01fab5658c
Junseo-Bae <74636948+Junseo-Bae@users.noreply.github.com>
2025-03-13 13:07:03 +0900

    Create 예제 7-1.c

---
+++ 예제 7-1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// for 문으로 10회 반복 출력 276p
+
+int main()
+{
+
+    int i;
+
+    for(i = 1; i <= 10; i++)
+    {
+        printf("%d\n", i);
+    }
+
+    return 0;
+}
