492ed0df8772e5e829ec2be42b9a092059c5a854
Junseo-Bae <74636948+Junseo-Bae@users.noreply.github.com>
2025-03-13 13:40:16 +0900

    Update 예제 7-7.c

--- 예제 7-7.c
+++ 예제 7-7.c
@@ -8,9 +8,9 @@ int main()
 
     int i, j;
 
-    for(i = 2; i <= 9; i++)
+    for(i = 2; i <= 9; i++) // 2 ~ 9단까지 출력
     {
-        for(j = 1; j <= 9; j++)
+        for(j = 1; j <= 9; j++) //곱하는 수
             printf("%d * %d = %d\n", i, j, i * j);
             printf("\n");
     }
