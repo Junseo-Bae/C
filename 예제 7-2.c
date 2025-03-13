f722ba5a8f32425d0c5239f8f46d6062c8bea2fb
Junseo-Bae <74636948+Junseo-Bae@users.noreply.github.com>
2025-03-13 13:08:47 +0900

    Create 예제 7-2.c

---
+++ 예제 7-2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// for 문으로 10회 반복 출력 후 카운트 횟수 출력 277p
+
+int main()
+{
+
+    int i;
+
+    for(i = 1; i <= 10; i++)
+    {
+        printf("%d\n", i);
+    }
+
+    printf("%d회", i);
+
+    return 0;
+}
