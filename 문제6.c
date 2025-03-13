fdffea5e88ed6a1238c7a9e310ab5c5a9f960cda
Junseo-Bae <74636948+Junseo-Bae@users.noreply.github.com>
2025-03-13 12:05:52 +0900

    Create 문제6.c

---
+++ 문제6.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i, j, k;
+
+    scanf("%d", &k);
+
+    for(j = 1; j <= k; j++)
+    {
+        for(i = 1; i <= k; i++)
+        {
+            if(j == 1 || j == k)
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+
+}
