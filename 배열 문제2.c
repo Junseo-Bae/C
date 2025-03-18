a008dcd455afb3459ff72233eb1169acca4cbb2e
Junseo-Bae <74636948+Junseo-Bae@users.noreply.github.com>
2025-03-18 11:31:09 +0900

    Create 배열 문제2.c

---
+++ 배열 문제2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+
+    int arr[10];
+    int i, n, index = 0;
+
+    printf("배열에 넣을 숫자를 입력하세요..\n");
+
+    for(i = 0; i < 10; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    
+    for(i = 0; i < 10; i++)
+    {
+        if(arr[i] > )
+        {
+            
+        }
+    }
+    
+    printf("가장 큰 수는 %d이고 인덱스는 %d입니다.");
+
+    return 0;
+}
