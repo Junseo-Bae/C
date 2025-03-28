e62a899a4d8910ac39f3780be6f3cbe02fe6a1aa
Junseo-Bae <74636948+Junseo-Bae@users.noreply.github.com>
2025-03-28 18:25:02 +0900

    Update 배열 문제2.c

--- 배열 문제2.c
+++ 배열 문제2.c
@@ -3,7 +3,7 @@
 int main() {
 
     int arr[10];
-    int i, index = 0;
+    int i, max, index;
 
     printf("배열에 넣을 숫자를 입력하세요..\n");
 
@@ -11,16 +11,19 @@ int main() {
     {
         scanf("%d", &arr[i]);
     }
-    
+
+    max = arr[0];
+
     for(i = 0; i < 10; i++)
     {
-        if(arr[i] > index)
+        if(arr[i] > max)
         {
-            index = 
+            max = arr[i];
+            index = i;
         }
     }
-    
-    printf("가장 큰 수는 %d이고 인덱스는 %d입니다.");
+
+    printf("가장 큰 수는 %d이고 인덱스는 %d입니다.", max, index);
 
     return 0;
 }
