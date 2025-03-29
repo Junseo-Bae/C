df89378fccdbfaffbe7cc2ec67ae6f4e86472628
Junseo-Bae <74636948+Junseo-Bae@users.noreply.github.com>
2025-03-29 16:03:15 +0900

    Create 문자열 1.c

---
+++ 문자열 1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <time.h>
+
+int main()
+{
+    char str[30];
+    int i, index = 0;
+
+    scanf("%s", str);
+
+    for(i = 0; i < 30; i++)
+    {
+        if(str[i] == '\0')
+        {
+            break;
+        }
+        else
+        {
+            index++;
+        }
+    }
+    printf("%d", index);
+
+    return 0;
+}
