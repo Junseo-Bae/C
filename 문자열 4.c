f8842b7e85ecdaedade9ed20e9ab950ca72afecb
Junseo-Bae <74636948+Junseo-Bae@users.noreply.github.com>
2025-04-02 09:29:19 +0900

    Create 문자열 4.c

---
+++ 문자열 4.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int len(char str[])
+{
+    int i, index = 0;
+
+    for(i = 0; i < 30; i++)
+    {
+        if(str[i] == '\0')
+        {
+            break;
+        }
+        else
+        {
+            index++;
+        }
+    }
+    return index;
+}
+
+int main()
+{
+    char str[30];
+    char find;
+    int i;
+
+    scanf("%s", str);
+    scanf("%c", &find);
+
+    for(i = 0; i < len; i++)
+    {
+        if(str[i] == find)
+        {
+            printf("%d", len(str));
+            break;
+        }
+    }
+
+
+    return 0;
+}
