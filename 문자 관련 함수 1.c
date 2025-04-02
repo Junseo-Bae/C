6dec5512b434c707006187a7cb14848a36f7bc8c
Junseo-Bae <74636948+Junseo-Bae@users.noreply.github.com>
2025-04-02 09:42:06 +0900

    Create 문자 관련 함수 1.c

---
+++ 문자 관련 함수 1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    char str[30];
+    int i;
+
+//    printf("%d\n", '0');
+
+    scanf("%s", str);
+
+    if(str[i] >= '0' && str[i] <= '9')
+    {
+        
+    }
+
+
+    return 0;
+}
