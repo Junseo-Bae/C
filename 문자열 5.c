e3a5963bd315229de8c8f80de152f2689fbdc8bc
Junseo-Bae <74636948+Junseo-Bae@users.noreply.github.com>
2025-04-04 11:24:54 +0900

    Update 문자열 5.c

--- 문자열 5.c
+++ 문자열 5.c
@@ -1,23 +1,5 @@
 #include <stdio.h>
 
-int len(char str[])
-{
-    int i, index = 0;
-
-    for(i = 0; i < 30; i++)
-    {
-        if(str[i] == '\0')
-        {
-            break;
-        }
-        else
-        {
-            index++;
-        }
-    }
-    return index;
-}
-
 int main()
 {
     char str1[30];
@@ -26,29 +8,20 @@ int main()
     scanf("%s", str1);
     scanf("%s", str2);
 
-    int i, temp;
-    int len1 = len(str1);
-    int len2 = len(str2);
+    int i;
+    int len1 = strlen(str1);
+    int len2 = strlen(str2);
 
-    if(len1 > len2)
-    {
-        temp = len1;
-        len1 = len2;
-        len2 = temp;
-    }
-
-    for(i = 0; i < temp; i++)
+    for(i = 0; i < len1; i++)
     {
         if(str1[i] != str2[i])
         {
             printf("모든 문자가 같지 않습니다.");
-            break;
-        }
-        else
-        {
-            printf("모든 문자가 같습니다.");
+            return 0;
         }
     }
+
+    printf("모든 문자가 같습니다.");
 
     return 0;
 }
