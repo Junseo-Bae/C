e81c86aecf994e355d6e286850d6941202487779
Junseo-Bae <74636948+Junseo-Bae@users.noreply.github.com>
2025-04-11 21:00:05 +0900

    Update 로또.c

--- 로또.c
+++ 로또.c
@@ -5,7 +5,8 @@
 int main()
 {
     int lotto[5][6];
-    int i, j, n;
+    int i, j, k, n;
+    int num, flag, temp;
     srand(time(NULL));
 
     while(1)
@@ -19,11 +20,45 @@ int main()
         printf("재입력\n");
     }
 
-    for(int i = 0; i < n; i++)
+    for(i = 0; i < n; i++)
     {
         for(j = 0; j < 6; j++)
         {
-            lotto[i][j] = rand() % 45 + 1;
+            num = rand() % 45 + 1;
+            flag = 0;
+
+            for(k = 0; k < j; k++)
+            {
+                if(lotto[i][k] == num)
+                {
+                    flag = 1;
+                }
+            }
+
+            if(flag)
+            {
+                j--;
+            }
+            else
+            {
+                lotto[i][j] = num;
+            }
+        }
+    }
+
+    for(i = 0; i < n; i++)
+    {
+        for(j = 0; j < 6 - 1; j++)
+        {
+            for(k = 0; k < 6 - 1 -j; k++)
+            {
+                if(lotto[i][k] > lotto[i][k+1])
+                {
+                    temp = lotto[i][k];
+                    lotto[i][k] = lotto[i][k+1];
+                    lotto[i][k+1] = temp;
+                }
+            }
         }
     }
 
