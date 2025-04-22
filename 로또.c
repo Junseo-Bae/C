9f7f08b04b85b3e799663e1f1453c8c376e49b98
Junseo-Bae <74636948+Junseo-Bae@users.noreply.github.com>
2025-04-23 08:30:24 +0900

    Update 로또.c

--- 로또.c
+++ 로또.c
@@ -5,14 +5,19 @@
 int main()
 {
     int lotto[5][6];
+    int win[6];
+    int bonus;
     int i, j, k, n;
-    int num, flag, temp;
+    int temp, num, flag;
     srand(time(NULL));
+    int brr[6] = {0};
+    int count = 0;
+    int bonus_count = 0;
 
     while(1)
     {
+        printf("게임 수 입력 (1~5): ");
         scanf("%d", &n);
-
         if(n >= 1 && n <= 5)
         {
             break;
@@ -22,35 +27,38 @@ int main()
 
     for(i = 0; i < n; i++)
     {
-        for(j = 0; j < 6; j++)
+        for(j = 0; j < 6;)
         {
             num = rand() % 45 + 1;
             flag = 0;
 
             for(k = 0; k < j; k++)
             {
-                if(lotto[i][k] == num)
+                if(brr[k] == num)
                 {
                     flag = 1;
+                    break;
                 }
             }
 
-            if(flag)
+            if(flag == 0)
             {
-                j--;
+                brr[j] = num;
+                j++;
             }
-            else
-            {
-                lotto[i][j] = num;
-            }
+        }
+
+        for(j = 0; j < 6; j++)
+        {
+            lotto[i][j] = brr[j];
         }
     }
 
     for(i = 0; i < n; i++)
     {
-        for(j = 0; j < 6 - 1; j++)
+        for(j = 0; j < 5; j++)
         {
-            for(k = 0; k < 6 - 1 -j; k++)
+            for(k = 0; k < 5 - j; k++)
             {
                 if(lotto[i][k] > lotto[i][k+1])
                 {
@@ -62,6 +70,60 @@ int main()
         }
     }
 
+    for(i = 0; i < 6;)
+    {
+        num = rand() % 45 + 1;
+        flag = 0;
+
+        for(j = 0; j < i; j++)
+        {
+            if(win[j] == num)
+            {
+                flag = 1;
+                break;
+            }
+        }
+
+        if(flag == 0)
+        {
+            win[i] = num;
+            i++;
+        }
+    }
+
+    for(i = 0; i < 5; i++)
+    {
+        for(j = 0; j < 5 - i; j++)
+        {
+            if(win[j] > win[j + 1])
+            {
+                temp = win[j];
+                win[j] = win[j + 1];
+                win[j + 1] = temp;
+            }
+        }
+    }
+
+    while(1)
+    {
+        bonus = rand() % 45 + 1;
+        flag = 0;
+
+        for(i = 0; i < 6; i++)
+        {
+            if(win[i] == bonus)
+            {
+                flag = 1;
+                break;
+            }
+        }
+
+        if(flag == 0)
+            break;
+    }
+
+    printf("\n");
+    printf("생성된 로또 번호: \n");
     for(i = 0; i < n; i++)
     {
         for(j = 0; j < 6; j++)
@@ -69,6 +131,73 @@ int main()
             printf("%d ", lotto[i][j]);
         }
         printf("\n");
+    }
+    printf("\n");
+
+    printf("당첨 번호: ");
+    for(i = 0; i < 6; i++)
+    {
+        printf("%d ", win[i]);
+    }
+    printf("\n");
+    printf("보너스 번호: %d\n", bonus);
+
+    for(i = 0; i < n; i++)
+    {
+        for(j = 0; j < 6; j++)
+        {
+            for(k = 0; k < 6; k++)
+            {
+                if(lotto[i][j] == win[k])
+                {
+
+                }
+            }
+        }
+    }
+
+    for(i = 0; i < n; i++)
+    {
+        for(j = 0; j < 6; j++)
+        {
+            for(k = 0; k < 6; k++)
+            {
+                if(lotto[i][j] == win[k])
+                {
+                    count++;
+                }
+            }
+            if(lotto[i][j] == bonus)
+            {
+                bonus_count = 1;
+            }
+        }
+    }
+
+    printf("결과 : ");
+    if(count == 6)
+    {
+        printf("1등\n");
+    }
+    else if(count == 5 && bonus_count == 1)
+    {
+        printf("2등\n");
+    }
+    else if(count == 5)
+    {
+        printf("3등\n");
+    }
+    else if(count == 4)
+    {
+        printf("4등\n");
+    }
+    else if(count == 3)
+    {
+        printf("5등\n");
+    }
+    else
+    {
+        printf("꽝");
     }
 
     return 0;
