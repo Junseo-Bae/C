34594ab913b9056bd6bdc28f26099c8fbd69df33
Junseo-Bae <74636948+Junseo-Bae@users.noreply.github.com>
2025-05-10 11:23:43 +0900

    Update malloc 로또 생성.c

--- malloc 로또 생성.c
+++ malloc 로또 생성.c
@@ -12,6 +12,20 @@ int main() {
     games = money / 5000;
 
     int** lotto = (int**)malloc(sizeof(int*)*games);
+    for (i = 0; i < games; i++)
+    {
+        lotto[i] = (int*)malloc(sizeof(int)*6);
+    }
+
+    for (i = 0; i < games; i++)
+    {
+        for (j = 0; j < 6; j++)
+        {
+            lotto[i][j] = rand() % 45 + 1;
+            printf("%d ", lotto[i][j]);
+        }
+        printf("\n");
+    }
 
     return 0;
 }
